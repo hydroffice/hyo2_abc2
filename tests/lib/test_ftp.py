8301bd30c85044a1fa93a9390b54d8fbb5362bbf
giumas <giuseppemasetti@gmail.com>
2024-10-20 10:06:16 +0200

    extended ftp tests

--- tests/lib/test_ftp.py
+++ tests/lib/test_ftp.py
@@ -1,12 +1,29 @@
+import os
 import unittest
 
+from hyo2.abc2.lib.testing import Testing
 from hyo2.abc2.lib.ftp import Ftp
 
 
 class TestABCLibFtp(unittest.TestCase):
 
     def test_init(self):
+        _ = Ftp(host="ftp.ccom.unh.edu")
+
+    def test_disconnect(self):
+        ftp = Ftp(host="ftp.ccom.unh.edu", show_progress=False, debug_mode=False)
+        ftp.disconnect()
+
+    def test_download(self):
+        root_folder = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
+        tp = Testing(root_folder=root_folder)
+        file_path = os.path.join(tp.output_data_folder(), "ccom.ico")
+
+        if os.path.exists(file_path):
+            os.remove(file_path)
+
         ftp = Ftp(host="ftp.ccom.unh.edu", password="test@gmail.com", show_progress=True, debug_mode=True)
+        ftp.get_file(file_src="/it/files/images/ccom.ico", file_dst=file_path, unzip_it=False)
         ftp.disconnect()
 
 
